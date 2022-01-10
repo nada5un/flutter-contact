1613fa878ca5e8b93237f498dd6c36b2c090c763
daeun <sung980524@naver.com>
2022-01-10 16:55:57 +0900

    ListView.builder, custom Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 //stless
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  var a = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -14,40 +16,65 @@ class MyApp extends StatelessWidget {
       //lp 단위
       //상중하로 나눔
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            a+=1;
+          },
+        ),
         appBar: AppBar(title:Text("앱임")),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child:Row(
-            children: [
-              Image.asset("imogi.png",width: 150),
-              //박스 하나만 꽉 채우기
-              Expanded(
-                child:
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)",style: TextStyle(fontWeight: FontWeight.bold),),
-                    Text("성동구 행당동 끌올 10분 전"),
-                    Text("210,000원"),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text("4")
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-      ),
-      ));
+        //목록
+        body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (c,i){
+            return ListTile(
+                leading: Image.asset("icon_no_child.png", height: 50,),
+                title: Text("홍길동")
+            );
+          },
+        ),
+        bottomNavigationBar: BottomApp(),
+      )
+    );
     //1. 예시 디자인 준비 (없으면 다른앱 배끼기)
     //2. 예시화면에 네모 그리기
     //3. 바깥 네모부터 하나하나 위젯으로
     //4. 마무리 디자인
+  }
+}
+
+class BottomApp extends StatelessWidget {
+  const BottomApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+//커스텀 클래스 => 재사용 많은 UI들, 큰 페이지들
+class ShopItem extends StatelessWidget {
+  //class 에 어떤 파라미터 넣을 수 있는지 정의
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  build(context) {
+    //레이아웃 리턴
+    return SizedBox(
+      child: Text("안녕"),
+    );
   }
 }
