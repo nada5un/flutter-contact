5caee31291eaf824f9e32a1ad4f03f2eeea5e4b2
daeun <sung980524@naver.com>
2022-01-13 12:15:44 +0900

    연락처 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp()
+    ));
 }
 
 //stful : 스테이트 만들기
@@ -14,41 +17,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var total = 3;
   var nameList = [
     "홍길동","길동","길길"
   ];
   var likesList = [0,0,0];
 
+  addA()=>setState((){
+    total++;
+  });
+
+  appendName(String name)=>setState((){
+      print(name);
+      nameList.add(name);
+  });
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      //lp 단위
-      //상중하로 나눔
-      home: Scaffold(
-        appBar: AppBar(title:Text("연락처앱")),
+  //context: 족보
+  build(context) {
+    return Scaffold(
+        appBar: AppBar(title:Text(total.toString())),
         //목록
         body: ListView.builder(
           itemCount: nameList.length,
           itemBuilder: (c,i){
             return ListTile(
-                leading: Text(likesList[i].toString()),
+                leading: Image.asset('icon_no_child.png'),
                 title: Text(nameList[i].toString()),
-                trailing :
-                TextButton(
-                  child: Text("좋아요"),
-                  //setState 해줘야 반영됨
-                  onPressed: (){
-                      setState(() {
-                        likesList[i]++;
-                      });
-                  },
-                ),
+                // trailing :
+                // TextButton(
+                //   child: Text("좋아요"),
+                //   //setState 해줘야 반영됨
+                //   onPressed: (){
+                //       setState(() {
+                //         likesList[i]++;
+                //       });
+                //   },
+                // ),
             );
           },
         ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("+"),
+          onPressed: (){
+            // print(context.findAncestorWidgetOfExactType<MaterialApp>());
+            showDialog(context: context, builder: (context){
+              return DialogUI(name:nameList,total:total,addA:addA,appendName:appendName);
+            });
+          },
+        ),
         bottomNavigationBar: BottomApp(),
-      )
     );
     //1. 예시 디자인 준비 (없으면 다른앱 배끼기)
     //2. 예시화면에 네모 그리기
@@ -89,6 +107,65 @@ class ShopItem extends StatelessWidget {
     //레이아웃 리턴
     return SizedBox(
       child: Text("안녕"),
+    );
+  }
+}
+
+//다이얼로그 클래스
+class DialogUI extends StatefulWidget {
+  //중괄호는 선택적 파라미터 {}
+  DialogUI({
+    Key? key,
+    this.name,
+    this.total,
+    required this.addA,
+    required this.appendName
+  }) : super(key: key);
+
+  //부모에서 온 값은 read-only가 좋음
+  final name;
+  final total;
+  var inputData = TextEditingController();
+  var inputData2 = '';
+
+  final Function() addA;
+  final Function(String name) appendName;
+
+  @override
+  State<DialogUI> createState() => _DialogUIState();
+}
+
+class _DialogUIState extends State<DialogUI> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)
+      ),
+      title: Text("Contact"),
+      content: TextField(
+        // onChanged: (text) {
+        //   widget.inputData2 = text;
+        //   print(text);
+        // },
+        controller: widget.inputData,
+        decoration: InputDecoration(hintText: "Text Field in Dialog"),
+      ),
+      actions: [
+        TextButton(onPressed: (){
+          //현재 페이지 제거
+          Navigator.pop(context);
+        },
+            child: Text("취소")
+        ),
+        TextButton(onPressed: (){
+          widget.appendName(widget.inputData.text);
+          widget.addA();
+          Navigator.pop(context);
+        },
+            child: Text("완료")
+        )
+      ],
     );
   }
 }
